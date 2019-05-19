273e6d30645957f9829655e2090dc53bf821024c
David Bratton <debratton@comcast.net>
2019-05-19 12:48:17 -0500

    Start Project

---
+++ TaskList/TaskList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by David E Bratton on 5/18/19.
+//  Copyright © 2019 David Bratton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
